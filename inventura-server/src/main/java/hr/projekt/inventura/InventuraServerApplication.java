bc4439ab142c9addc427777c5f8ba0576a57011f
Ivan Prskalo <ivan-vulgaris@hotmail.com>
2020-03-14 11:27:36 +0100

    Initial commit

---
+++ inventura-server/src/main/java/hr/projekt/inventura/InventuraServerApplication.java
@@ -0,0 +1,13 @@
+package hr.projekt.inventura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventuraServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventuraServerApplication.class, args);
+    }
+
+}
---
+++ inventura-server/src/test/java/hr/projekt/inventura/InventuraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.projekt.inventura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventuraServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
